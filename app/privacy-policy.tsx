53339d042dec5b9263976180653ef50e4ccd5c84
taylorbaile0703-svg <taylor.baile0703@gmail.com>
2026-03-21 13:41:26 -0400

    Restore privacy policy screen

--- app/privacy-policy.tsx
+++ app/privacy-policy.tsx
@@ -1,7 +1,13 @@
+// Privacy Policy Screen
 import React from 'react';
 
-const PrivacyPolicy: JSX.Element[] = [
-    // Privacy policy content
-];
+const PrivacyPolicy: React.FC = (): JSX.Element[] => {
+    return [
+        <div key="privacy-policy">
+            <h1>Privacy Policy</h1>
+            <p>Your privacy policy content goes here.</p>
+        </div>
+    ];
+};
 
 export default PrivacyPolicy;
